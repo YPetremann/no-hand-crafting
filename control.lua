d6d5e6d1dc8173da259b06dff5fb06778ef282cc
Y.Petremann <Y.Petremann@gmail.com>
2015-11-22 02:48:57 +0100

    Update for 0.12.11

--- control.lua
+++ control.lua
@@ -1,12 +1,12 @@
 require "defines"
 
-game.on_init(function()
+script.on_init(function()
   pcall(function()
     game.player.insert{name = "burner-assembling-machine", count = 1}
   end)
 end)
 
-game.on_event(defines.events.on_player_created, function(event)
+script.on_event(defines.events.on_player_created, function(event)
   pcall(function()
     game.get_player(event.player_index).insert{name="burner-assembling-machine", count=1}
   end)

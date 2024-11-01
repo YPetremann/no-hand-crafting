3c0a78de9d6e089737727d0f1f385f4911ae3021
YPetremann <YPetremann@gmail.com>
2024-11-02 00:39:44 +0100

    Update changelog and migrate to version 2.0.0 with new features for burner assembling machine

--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -1,16 +1,5 @@
-function Set (list)
-  local set = {}
-  for _, l in ipairs(list) do set[l] = true end
-  return set
-end
-local crafting_categories = Set(data.raw.player.player.crafting_categories)
-local crafting_cap = 0
-for i, obj in pairs(data.raw["recipe"]) do
-  if (crafting_categories[obj.category] or obj.category == "" or obj.category == nil) then
-    if #obj.ingredients > crafting_cap then
-      crafting_cap = #obj.ingredients
-    end
-  end
-end
-data.raw["assembling-machine"]["burner-assembling-machine"].ingredient_count = crafting_cap
-data.raw.player.player.crafting_categories = nil
+local burner_assembling_machine = data.raw["assembling-machine"]["burner-assembling-machine"]
+local character = data.raw.character.character
+
+burner_assembling_machine.crafting_categories = character.crafting_categories
+character.crafting_categories = { "no-hand-crafting" }
--- data.lua
+++ data.lua
@@ -1,86 +1,56 @@
-data:extend({
-  {
-    type = "assembling-machine",
-    name = "burner-assembling-machine",
-    icon = "__no-hand-crafting__/graphics/icons/burner-assembling-machine.png",
-    flags = {"placeable-neutral", "placeable-player", "player-creation"},
-    minable = {hardness = 0.2, mining_time = 0.5, result = "burner-assembling-machine"},
-    max_health = 200,
-    corpse = "big-remnants",
-    dying_explosion = "medium-explosion",
-    resistances = {{type = "fire", percent = 70}},
-    collision_box = {{-1.2, -1.2}, {1.2, 1.2}},
-    selection_box = {{-1.5, -1.5}, {1.5, 1.5}},
-    fast_replaceable_group = "assembling-machine",
-    animation = {
-      filename = "__no-hand-crafting__/graphics/entity/burner-assembling-machine/burner-assembling-machine.png",
-      priority="high",
-      width = 99,
-      height = 102,
-      frame_count = 32,
-      line_length = 8,
-      shift = {0.25, -0.1}
-    },
-    crafting_categories = {"crafting"},
-    crafting_speed = 0.25,
-    energy_source = {
-      type = "burner",
-      effectivity = 1,
-      fuel_inventory_size = 1,
-      emissions = 0.01,
-      smoke =
-      {
-        {
-          name = "smoke",
-          deviation = {0.1, 0.1},
-          frequency = 0.5,
-          position = {0, 0},
-          starting_vertical_speed = 0.05
-        }
-      }
-    },
-    energy_usage = "90kW",
-    ingredient_count = 6,
-    open_sound = { filename = "__base__/sound/machine-open.ogg", volume = 0.85 },
-    close_sound = { filename = "__base__/sound/machine-close.ogg", volume = 0.75 },
-    working_sound = {
-      sound = {
-        {
-          filename = "__base__/sound/assembling-machine-t1-1.ogg",
-          volume = 0.8
-        },
-        {
-          filename = "__base__/sound/assembling-machine-t1-2.ogg",
-          volume = 0.8
-        },
-      },
-      idle_sound = { filename = "__base__/sound/idle1.ogg", volume = 0.6 },
-      apparent_volume = 1.5,
+local bam = table.deepcopy(data.raw["assembling-machine"]["assembling-machine-1"])
+bam.name = "burner-assembling-machine"
+bam.icon = "__no-hand-crafting__/graphics/icons/burner-assembling-machine.png"
+bam.minable.result = "burner-assembling-machine"
+bam.graphics_set.animation.layers[1].filename =
+"__no-hand-crafting__/graphics/entity/burner-assembling-machine/burner-assembling-machine.png"
+bam.crafting_speed = 0.25
+bam.energy_usage = "90kW"
+bam.ingredient_count = nil
+bam.enable_logistic_control_behavior = false
+bam.circuit_connector = nil
+bam.draw_circuit_wires = false
+bam.circuit_wire_max_distance = 0
+bam.energy_source = {
+  type = "burner",
+  effectivity = 1,
+  fuel_inventory_size = 1,
+  emissions = 0.01,
+  smoke = {
+    {
+      name = "smoke",
+      deviation = { 0.1, 0.1 },
+      frequency = 0.5,
+      position = { 0, 0 },
+      starting_vertical_speed = 0.05
     }
-  },
+  }
+}
+
+data:extend({
+  bam,
   {
     type = "item",
     name = "burner-assembling-machine",
     icon = "__no-hand-crafting__/graphics/icons/burner-assembling-machine.png",
-    flags = {"goes-to-quickbar"},
     subgroup = "production-machine",
     order = "a[assembling-machine-1]",
     place_result = "burner-assembling-machine",
     stack_size = 50
   },
   {
-    type = "recipe-category",
-    name = "basic-crafting"
+    type = "recipe",
+    name = "burner-assembling-machine",
+    ingredients = {
+      { type = "item", name = "stone-furnace",   amount = 1 },
+      { type = "item", name = "iron-gear-wheel", amount = 5 }
+    },
+    results = {
+      { type = "item", name = "burner-assembling-machine", amount = 1 }
+    },
   },
   {
     type = "recipe-category",
     name = "no-hand-crafting"
   },
-  {
-    type = "recipe",
-    name = "burner-assembling-machine",
-    ingredients = {{"stone-furnace", 1}, {"iron-gear-wheel", 5}},
-    result = "burner-assembling-machine"
-  },
-
 })
